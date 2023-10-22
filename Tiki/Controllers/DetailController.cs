59154e4d76fc97fa859f387d247334ba6c172f8e
Jason Huynh <jasonn0203@gmail.com>
2023-10-22 13:55:45 +0700

    detail - FE - unfinish

---
+++ Tiki/Controllers/DetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tiki.Controllers
+{
+    public class DetailController : Controller
+    {
+        // GET: Detail
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
