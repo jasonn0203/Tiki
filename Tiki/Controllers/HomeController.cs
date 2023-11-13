ff76ba4a69e902c5f28d42deb1334713a8646640
Jason Huynh <jasonn0203@gmail.com>
2023-11-13 22:21:02 +0700

    sort product - BE

--- Tiki/Controllers/HomeController.cs
+++ Tiki/Controllers/HomeController.cs
@@ -19,11 +19,35 @@ namespace Tiki.Controllers
         }
 
 
-        public ActionResult ProductByCategory(string tenPL)
+        public ActionResult ProductByCategory(string tenPL, string sortOrder)
         {
+            // Giá trị mặc định ban đầu 
             var spTheoPLList = db.SanPhams.Where(sp => sp.PhanLoaiSP.TenPhanLoai == tenPL).ToList();
+            ViewBag.PriceDesc = false;
+            ViewBag.PriceAsc = false;
+            switch (sortOrder)
+            {
+                case "PriceAsc":
+                    spTheoPLList = spTheoPLList.OrderBy(s => s.Gia).ToList();
+                    ViewBag.PriceDesc = false;
+                    ViewBag.PriceAsc = true;
+                    break;
+                case "PriceDesc":
+                    spTheoPLList = spTheoPLList.OrderByDescending(s => s.Gia).ToList();
+                    ViewBag.PriceAsc = false;
+                    ViewBag.PriceDesc = true;
+                    break;
+                default:
+                    // Sort products by product name by default
+                    spTheoPLList = spTheoPLList.OrderBy(s => s.TenSanPham).ToList();
+                    break;
+            }
+            ViewBag.TenPL = tenPL;
+
             return View(spTheoPLList);
         }
+
+
 
 
 
